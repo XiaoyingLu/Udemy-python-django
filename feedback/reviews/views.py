38f945b0106ace2ad480d2a755dbb9b9a6e9c2aa
amberlu <xiaoyinglu59@gmail.com>
2024-01-21 17:33:31 -0400

    used session to store user's favorite in feedback

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -74,3 +74,17 @@ class ReviewsListView(ListView):
 class SingleReviewView(DetailView):
     template_name = "reviews/review_detail.html"
     model = Review
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        favorite_id = self.request.session.get("favorite_review")
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+        return context
+
+
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST["review_id"]
+        request.session["favorite_review"] = review_id
+        return HttpResponseRedirect("/reviews/" + review_id)
